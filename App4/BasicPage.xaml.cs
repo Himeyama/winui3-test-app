c7d5d5788e9597eee1cb40fc18ac5d514c28a247
Murata Mitsuharu <hikari.photon+dev@gmail.com>
2021-12-19 11:52:51 +0900

    OCR

---
+++ App4/BasicPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace App4
+{
+    public class BasicPage : Page
+    {
+        internal StackPanel atb;
+        internal StackPanel at;
+        internal TextBlock att;
+
+        public BasicPage()
+        {
+            ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        }
+
+        internal void MicaTitle(StackPanel AppTitleBar, StackPanel AppTitle, TextBlock AppTitleText)
+        {
+            atb = AppTitleBar;
+            at = AppTitle;
+            att = AppTitleText;
+
+            //デフォルトのタイトルバーを非表示
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            UpdateTitleBarLayout(coreTitleBar);
+
+            // XAML要素をドラッグ可能な領域として設定
+            Window.Current.SetTitleBar(at);
+
+            // オーバーレイされたキャプションコントロールのサイズが変更された場合のハンドラーを登録
+            // たとえば、アプリが別のDPIの画面に移動した場合
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+
+            // タイトルバーの表示が変更されたときのハンドラーを登録
+            // たとえば、タイトルバーがフルスクリーンモードで呼び出された場合
+            coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
+
+            // ウィンドウがフォーカスを変更したときのハンドラーを登録
+            Window.Current.Activated += Current_Activated;
+        }
+
+        internal void UpdateTitleBarLayout(CoreApplicationViewTitleBar coreTitleBar)
+        {
+            // システムサイズの変更に対応するために、必要に応じてタイトルバーのコントロールサイズを更新
+            //atb.Height = coreTitleBar.Height;
+            atb.Height = 60;
+
+            // カスタムタイトルバーがウィンドウのキャプションコントロールと重ならないように
+            Thickness currMargin = atb.Margin;
+            atb.Margin = new Thickness(currMargin.Left, currMargin.Top, coreTitleBar.SystemOverlayRightInset, currMargin.Bottom);
+        }
+
+        internal void Current_Activated(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
+        {
+            SolidColorBrush defaultForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorPrimaryBrush"];
+            SolidColorBrush inactiveForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorDisabledBrush"];
+
+            if (e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.Deactivated)
+            {
+                att.Foreground = inactiveForegroundBrush;
+            }
+            else
+            {
+                att.Foreground = defaultForegroundBrush;
+            }
+        }
+
+
+        internal void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            UpdateTitleBarLayout(sender);
+        }
+
+        private void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (sender.IsVisible)
+            {
+                atb.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                atb.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
--- App4/MainPage.xaml.cs
+++ App4/MainPage.xaml.cs
@@ -1,100 +1,81 @@
 ﻿using System;
-using Windows.ApplicationModel.Core;
-using Windows.UI;
-using Windows.UI.ViewManagement;
-using Windows.UI.WindowManagement;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Windows.Globalization;
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Media;
-
-// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+using Windows.UI.Xaml.Media.Imaging;
 
 namespace App4
 {
-    /// <summary>
-    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage : BasicPage
     {
-        public MainPage()
+        string sentence;
+        IReadOnlyList<Language> langs;
+
+        public MainPage(): base()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            MicaTitle(AppTitleBar, AppTitle, AppTitleText);
+            //Log.debug(OcrEngine.AvailableRecognizerLanguages);
 
-            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
-
-            titleBar.ButtonBackgroundColor = Colors.Transparent;
-            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
-
-            // Hide default title bar.
-            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
-            coreTitleBar.ExtendViewIntoTitleBar = true;
-            UpdateTitleBarLayout(coreTitleBar);
-
-            // Set XAML element as a draggable region.
-            Window.Current.SetTitleBar(AppTitleBar);
-
-            // Register a handler for when the size of the overlaid caption control changes.
-            // For example, when the app moves to a screen with a different DPI.
-            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
-
-            // Register a handler for when the title bar visibility changes.
-            // For example, when the title bar is invoked in full screen mode.
-            coreTitleBar.IsVisibleChanged += CoreTitleBar_IsVisibleChanged;
-
-            //Register a handler for when the window changes focus
-            Window.Current.Activated += Current_Activated;
-        }
-
-        private void UpdateTitleBarLayout(CoreApplicationViewTitleBar coreTitleBar)
-        {
-            // Update title bar control size as needed to account for system size changes.
-            AppTitleBar.Height = coreTitleBar.Height;
-
-            // Ensure the custom title bar does not overlap window caption controls
-            Thickness currMargin = AppTitleBar.Margin;
-            AppTitleBar.Margin = new Thickness(currMargin.Left, currMargin.Top, coreTitleBar.SystemOverlayRightInset, currMargin.Bottom);
-        }
-
-        private void Current_Activated(object sender, Windows.UI.Core.WindowActivatedEventArgs e)
-        {
-            SolidColorBrush defaultForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorPrimaryBrush"];
-            SolidColorBrush inactiveForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorDisabledBrush"];
-
-            if (e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.Deactivated)
+            langs = OcrEngine.AvailableRecognizerLanguages;
+            for(int i = 0; i < langs.Count; i++)
             {
-                AppTitle.Foreground = inactiveForegroundBrush;
-            }
-            else
-            {
-                AppTitle.Foreground = defaultForegroundBrush;
+                Language language = langs[i];
+                // English (United States)
+                string langNativeName = language.NativeName;
+
+                // en-US
+                string lang = language.LanguageTag;
+
+                SelectLang.Items.Add(langNativeName);
             }
         }
 
-
-        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        private async void OpenFile(object sender, RoutedEventArgs e)
         {
-            UpdateTitleBarLayout(sender);
+            FileOpenPicker filePicker = new();
+            filePicker.FileTypeFilter.Add(".png");
+            StorageFile file = await filePicker.PickSingleFileAsync();
+            //string fileName = file.Path;
+
+            if (file != null)
+            {
+                using (IRandomAccessStream fileStream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                {
+                    BitmapImage bitmapImage = new();
+                    await bitmapImage.SetSourceAsync(fileStream);
+                    inputImage.Source = bitmapImage;
+                }
+
+                // 言語
+                Language lang = langs[SelectLang.SelectedIndex];
+
+
+                // OCR
+                SoftwareBitmap softwareBitmap;
+                using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+                {
+                    BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+                    softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+                }
+                //OcrEngine ocrEngine = OcrEngine.TryCreateFromUserProfileLanguages();
+                OcrEngine ocrEngine = OcrEngine.TryCreateFromLanguage(lang);
+                OcrResult result = await ocrEngine.RecognizeAsync(softwareBitmap);
+                sentence = result.Text;
+                text.Text = sentence;
+            }
         }
 
-        private void CoreTitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args)
+        private void ExitApp(object sender, RoutedEventArgs e)
         {
-            if (sender.IsVisible)
-            {
-                AppTitleBar.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                AppTitleBar.Visibility = Visibility.Collapsed;
-            }
-        }
-
-        private async void About_Version(object sender, RoutedEventArgs e)
-        {
-            BlankPage1 frame = new BlankPage1();
-            var appWindow = await AppWindow.TryCreateAsync();
-            ElementCompositionPreview.SetAppWindowContent(appWindow, frame);
-            await appWindow.TryShowAsync();
+            Application.Current.Exit();
         }
     }
 }
